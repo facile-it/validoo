44b0a1d1392ff6814dfb7d5144b3d8fbb2233fcc
alessandro <alessandro.manno@facile.it>
2017-01-31 16:33:40 +0100

    fixed "mp3" is_filename issue

--- src/Validoo/Validator.php
+++ src/Validoo/Validator.php
@@ -301,7 +301,7 @@ class Validator
      */
     protected static function is_filename($input): bool
     {
-        return preg_match('/^[A-Za-z0-9-_]+[.]{1}[A-Za-z]+$/', $input);
+        return preg_match('/^[A-Za-z0-9-_]+[.]{1}[A-Za-z0-9]+$/', $input);
     }
 
     /**
